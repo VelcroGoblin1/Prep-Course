42ade512fe41e9f3a634c40ff2219f83c2a595e0
Nicolás Dandolo <bsqfntsm@gmail.com>
2022-06-28 00:31:30 -0300

    tareajsIII

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -247,6 +247,14 @@ function breakStatement(numero) {
   //devolver: "Se interrumpió la ejecución"
   //Pista: usá el statement 'break'
   // Tu código:
+  var numerosIterados = [];
+  for (var i = 0; i < 10; i++) {
+    if (numero === i) {
+      return "Se interrumpió la ejecución";
+      break;
+    }
+      numerosIterados.push(numero[i]);
+  }
 }
 
 
@@ -257,6 +265,13 @@ function continueStatement(numero) {
   //Cuando el número de iteraciones alcance el valor 5, no se suma en ese caso y se continua con la siguiente iteración
   //Pista: usá el statement 'continue'
   // Tu código:
+  var numerosIterados = [];
+  for (var i = 0; i < 10; i++) {
+    if (i === 5) {
+      continue;
+    }
+      numerosIterados.push(numero[i]);
+  }
 }
 
 
