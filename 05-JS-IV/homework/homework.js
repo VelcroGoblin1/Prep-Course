e63e66fd4d1c9ed35b407a5ebe24e61c5b7287e3
Nicolás Dandolo <bsqfntsm@gmail.com>
2022-07-05 11:34:14 -0300

    tareaJSIV

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -22,8 +22,9 @@ function agregarPropiedad (objeto, property) {
   // NOTA: El nombre de la propiedad no es "propiedad", el nombre es el valor del argumento llamado "property" (una cadena/string)
   // Tu código:
   var objeto = {
-    property: null
   }
+
+  objeto.property = null;
   return objeto;
 }
 
