12c1cc3e78a86000c94412516f19a94c7a535791
Nicolás Dandolo <bsqfntsm@gmail.com>
2022-07-05 11:40:03 -0300

    tareaJSIV

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -21,9 +21,6 @@ function agregarPropiedad (objeto, property) {
   // Devuelve el objeto
   // NOTA: El nombre de la propiedad no es "propiedad", el nombre es el valor del argumento llamado "property" (una cadena/string)
   // Tu código:
-  var objeto = {
-  }
-
   objeto[property] = null;
   return objeto;
 }
@@ -33,6 +30,7 @@ function invocarMetodo (objeto, metodo) {
   // Invoca ese método
   // Nada necesita ser devuelto ("returned")
   // Tu código:
+  objeto.metodo();
 }
 
 function multiplicarNumeroDesconocidoPorCinco (objetoMisterioso) {
